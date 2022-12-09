442287d8e987ac16acf921757ff8ee0f2eaa0a42
balogun oluwaseun <vibrator1759@gmail.com>
2022-12-09 02:11:45 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Main print file using printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,, followed by a new line.
+
+Use the function printf,\n");
+	return (0);
+	
+}	
