166306a4f833dea2009e4748c9ea50a75d152842
balogun oluwaseun <vibrator1759@gmail.com>
2022-12-08 20:47:53 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /** 
- * main -print a line of code using puts
- * return; 0
- * */
+* main -print a line of code using puts
+* return (0);0
+*/
 int main(void)
 {	
-	puts ("\" peogramming is like building a multilingual puzzle");
-	return (0);
+ puts("\"Programming is like building a multilingual puzzle,");
+ return (0);
 }	
