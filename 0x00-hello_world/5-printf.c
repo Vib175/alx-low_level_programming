a6ccdd91927383ce403920002fef1a61440ac724
balogun oluwaseun <vibrator1759@gmail.com>
2022-12-10 00:01:27 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
- * Main print file using printf
+ * main - Entry point
  *
- * Return: 0
+ *  Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,, followed by a new line.
-
-Use the function printf,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-	
-}	
+}
