0aa1fa44e92183a00b72b14b95096b8bad449133
balogun oluwaseun <vibrator1759@gmail.com>
2022-12-19 12:01:48 -0800

    0. 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
+
 /**
- * reset_to_98 - pointing to an int parame
- * @n: - int to update
- *
- * Return: always (0)
+ * reset_to_98 - updates value of  n to 98
+ * @n: param
  */
 
 void reset_to_98(int *n)
 {
-
 	*n = 98;
 }
